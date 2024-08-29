5d1f492e9ad23e4f7d737f299b2038e8b5b020a8
Trey Hunner <trey@treyhunner.com>
2024-08-29 11:20:52 -0700

    Add docstrings

--- grudge_cabinet/tests.py
+++ grudge_cabinet/tests.py
@@ -2,13 +2,18 @@ from django.test import TestCase
 from .models import Grudge, Story
 
 class GrudgeModelTest(TestCase):
+
+    """Tests for Grudge model."""
+
     def setUp(self):
+        """Create a new Story."""
         self.story = Story.objects.create(
             title="Test Grudge Story",
             origin="This is a test origin story for the grudge."
         )
 
     def test_grudge_carat_max_values(self):
+        """Test Grudge with maximum value for the carat property."""
         grudge = Grudge.objects.create(
             origin_story=self.story,
             grudgee_intention=Grudge.Intention.DEFINITELY_BAD,
@@ -23,7 +28,6 @@ class GrudgeModelTest(TestCase):
             grudge_easily_forgiven=Grudge.EasilyForgiven.NO,
             grudgee_significance=Grudge.GrudgeeSignificance.MASSIVELY
         )
-
         expected_carat = (
             3 +  # grudgee_intention
             3 +  # grudgee_knowledge
@@ -37,10 +41,10 @@ class GrudgeModelTest(TestCase):
             0 +  # grudge_easily_forgiven
             4    # grudgee_significance
         )
-
         self.assertEqual(grudge.carat, expected_carat)
 
     def test_grudge_carat_medium_values(self):
+        """Test Grudge with middle-of-the-road carat value."""
         grudge = Grudge.objects.create(
             origin_story=self.story,
             grudgee_intention=Grudge.Intention.POSSIBLY_BAD,
@@ -55,7 +59,6 @@ class GrudgeModelTest(TestCase):
             grudge_easily_forgiven=Grudge.EasilyForgiven.YES,
             grudgee_significance=Grudge.GrudgeeSignificance.QUITE_A_LOT
         )
-
         expected_carat = (
             2 +  # grudgee_intention
             2 +  # grudgee_knowledge
@@ -69,10 +72,10 @@ class GrudgeModelTest(TestCase):
             -1 + # grudge_easily_forgiven
             2    # grudgee_significance
         )
-
         self.assertEqual(grudge.carat, expected_carat)
 
     def test_grudge_carat_new_grudge(self):
+        """Test that new Grudge with missing values has a None-value carat."""
         grudge = Grudge()
         self.assertIsNone(grudge.carat)
 
